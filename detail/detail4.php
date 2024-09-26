3f402f5511c0f6ce322dc5b0cbf51f80f96ac347
Mai Hùng <149920214+MaiHung2804@users.noreply.github.com>
2024-09-27 00:31:11 +0700

    Add files via upload

---
+++ detail/detail4.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Music for Life</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary shadow p-3 bg-white rounded">
+            <div class="container-fluid">
+                <div class="my-logo">
+                    <a class="navbar-brand" href="#">
+                        <img src="../../../../assets/images/logo2.png" alt="" class="img-fluid">
+                    </a>
+                </div>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="../../../../public/index.php">Trang chủ</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="../../login/login.php">Đăng nhập</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Nội dung cần tìm" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Tìm</button>
+                </form>
+                </div>
+            </div>
+        </nav>
+
+    </header>
+    <main class="container mt-5">
+        <!-- <h3 class="text-center text-uppercase mb-3 text-primary">CẢM NHẬN VỀ BÀI HÁT</h3> -->
+       
+                <div class="row mb-5">
+                    <div class="col-sm-4">
+                        <img src="../../../../assets/images/songs/phoipha.jpg" class="img-fluid" alt="...">
+                    </div>
+                    <div class="col-sm-8">
+                        <h5 class="card-title mb-2">
+                            <a href="../../../../assets/images/songs/phoipha.jpg" class="text-decoration-none">Phôi pha</a>
+                        </h5>
+                        <p class="card-text"><span class=" fw-bold">Bài hát: </span>Phôi pha</p>
+                        <p class="card-text"><span class=" fw-bold">Thể loại: </span>Nhạc trữ tình</p>
+                        <p class="card-text"><span class=" fw-bold">Tóm tắt: </span>"Phôi Pha" là một ca khúc đầy chất thơ, khắc họa sâu sắc vẻ đẹp mong manh và phù du của cuộc đời. Qua những câu hát nhẹ nhàng, trầm buồn, tác giả đã vẽ nên một bức tranh về thời gian trôi qua nhanh chóng, những kỷ niệm dần phai mờ và sự cô đơn của con người trước quy luật vô thường của cuộc sống.</p>
+                        <p class="card-text"><span class=" fw-bold">Nội dung: </span>Bài hát xoay quanh chủ đề về sự vô thường của thời gian và cuộc đời. Qua những hình ảnh ẩn dụ như "gió qua", "mây trôi", "bàn chân ai rất nhẹ tựa hồn những năm xưa", tác giả gợi lên cảm giác về sự trôi chảy của thời gian, sự thay đổi không ngừng của cuộc sống. Con người, dù có cố gắng níu giữ, cũng không thể ngăn cản dòng chảy của thời gian.</p>
+                        <p class="card-text"><span class=" fw-bold">Tác giả: </span>Trịnh Công Sơn</p>
+
+                    </div>          
+        </div>
+    </main>
+    <footer class="bg-white d-flex justify-content-center align-items-center border-top border-secondary  border-2" style="height:80px">
+        <h4 class="text-center text-uppercase fw-bold">TLU's music garden</h4>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+</html>
